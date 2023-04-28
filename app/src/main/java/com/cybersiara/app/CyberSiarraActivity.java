37d323d0d61cb45ef20c546010bcd8118a47fc03
Dhruv <shineinfosoft21@gmail.com>
2023-04-28 14:52:46 +0530

    intial commit

---
+++ app/src/main/java/com/cybersiara/app/CyberSiarraActivity.java
@@ -0,0 +1,18 @@
+package com.cybersiara.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CyberSiarraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
